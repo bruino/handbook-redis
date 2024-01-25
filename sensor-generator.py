b8237dfdb41d5ab68bf94f9feeb39c8cb95531d4
bruino <bruno.sarverry@gmail.com>
2024-01-25 16:29:51 -0300

    fixes

--- sensor-generator.py
+++ sensor-generator.py
@@ -4,20 +4,22 @@ from faker import Faker
 import redis
 
 fake = Faker()
-redis_client = redis.StrictRedis(host='localhost', port=6379, db=0)
+redis_client = redis.StrictRedis(host="redis-db", port=6379, db=0)
+
 
 def generate_sensor_data():
     sensor_data = {
-        'timestamp': int(time.time()),
-        'sensor_id': fake.uuid4(),
-        'temperature': fake.random_int(10, 30),
-        'humidity': fake.random_int(30, 70),
+        "timestamp": int(time.time()),
+        "sensor_id": fake.uuid4(),
+        "temperature": fake.random_int(10, 30),
+        "humidity": fake.random_int(30, 70),
     }
     return json.dumps(sensor_data)
+
 
 if __name__ == "__main__":
     while True:
         data = generate_sensor_data()
-        redis_client.publish('sensor_data_channel', data)
+        redis_client.publish("sensor_data_channel", data)
         print(f"Data sent: {data}")
         time.sleep(1)
